4b962bbae2db7bfb5ac3da2d2d29f88ba3112a14
Mackley <mackleycarmonaalvarez@gmail.com>
2019-01-07 18:13:18 +0000

    Actulice la contraseña

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,6 @@ let urlDB;
 if(process.env.NODE_ENV == 'dev'){
   urlDB='mongodb://localhost:27017/cafe';
 } else{
-  urlDB='mongodb://cafe-user:#cafe-user2019@ds019058.mlab.com:19058/cafe'
+  urlDB='mongodb://cafe-user:123456MAc@ds019058.mlab.com:19058/cafe'
 }
 process.env.URLDB=urlDB;
